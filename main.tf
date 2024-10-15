70f0c38611fe17c5884bcc6d466ecb09602ae5eb
nguyenhung1402 <21522118@ms.uit.edu.vn>
2024-10-15 22:06:52 +0700

    Update README

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 
 module "vpc" {
   source = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_block
 }
 
 module "subnet" {
   source             = "./modules/subnet"
   vpc_id             = module.vpc.vpc_id
-  public_cidr_block  = "10.0.1.0/24"
-  private_cidr_block = "10.0.2.0/24"
-  availability_zone  = "ap-southeast-1a"
+  public_cidr_block  = var.public_cidr_block
+  private_cidr_block = var.private_cidr_block
 }
 
 module "nat_gateway" {
@@ -20,19 +19,19 @@ module "nat_gateway" {
 module "security_group" {
   source                  = "./modules/security_group"
   vpc_id                  = module.vpc.vpc_id
-  allowed_ip              = "0.0.0.0/0"
+  allowed_ip              = var.allowed_ip
   public_security_group_id = module.security_group.public_security_group_id
 }
 
 module "ec2" {
   source                = "./modules/ec2"
-  ami                   = "ami-01811d4912b4ccb26"
-  instance_type         = "t2.micro"
+  ami                   = var.ami
+  instance_type         = var.instance_type
   public_subnet_id      = module.subnet.public_subnet_id
   private_subnet_id     = module.subnet.private_subnet_id
   public_security_group = module.security_group.public_security_group_id
   private_security_group = module.security_group.private_security_group_id
-  key_name              = "AWS"
+  key_name              = var.key_name
 }
 
 module "route_table" {
--- variables.tf
+++ variables.tf
@@ -2,4 +2,47 @@ variable "ami" {
   description = "ID của AMI"
   type        = string
   default     = "ami-01811d4912b4ccb26"
+  nullable = false
+}
+
+variable "allowed_ip" {
+  description = "IP được phép truy cập"
+  type        = string 
+  default = "0.0.0.0/0"
+  nullable = false
+}
+
+variable "instance_type" {
+  description = "Loại instance"
+  type        = string 
+  default = "t2.micro"
+  nullable = false
+}
+
+variable "key_name" {
+  description = "Tên của key pair"
+  type        = string 
+  default = "AWS"
+  nullable = false
+}
+
+variable "cidr_block" {
+  description = "CIDR block cho VPC"
+  type        = string 
+  default = "10.0.0.0/16"
+  nullable = false
+}
+
+variable "public_cidr_block" {
+  description = "CIDR block cho Public Subnet"
+  type        = string 
+  default = "10.0.1.0/24"
+  nullable = false
+}
+
+variable "private_cidr_block" {
+  description = "CIDR block cho Private Subnet"
+  type        = string 
+  default = "10.0.2.0/24"
+  nullable = false
 }
